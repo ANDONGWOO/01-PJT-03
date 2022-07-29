1f0fc283b99611ad3e5e5c4b3dad67f0a42e2c4e
an dong woo <akskwha@naver.com>
2022-07-29 18:27:09 +0900

    0729모의고사

--- 1회차/안동우/0_최빈수구하기.py
+++ 1회차/안동우/0_최빈수구하기.py
@@ -1,3 +1,17 @@
 import sys
 
 sys.stdin = open("_최빈수구하기.txt")
+#학생1000명
+#제약 0이상 100이하
+#최빈수 가장많이중복 높은 점수가 최빈수?
+#점수는 문자열  
+#중복 카운트 그중 최대 큰 수 출력
+# print(f"#{} {}".format(,))
+T=int(input())
+T_num=int(input())
+a=list(int,input().split())
+b=set(a)
+
+for z in T_num:#테스트 번호
+    for i in a:
+      print(b)
--- 1회차/안동우/1_소득불균형.py
+++ 1회차/안동우/1_소득불균형.py
@@ -1,3 +1,12 @@
 import sys
 
 sys.stdin = open("_소득불균형.txt")
+
+T=int(input())
+T_num=int(input())
+N_num=int(input()).split()
+a=int(N_num)
+for z in T_num:
+    
+    for i in a:
+        print(a)
